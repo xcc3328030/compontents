ab8c11a95757b5acf9899659aecb792072a5201c
xcc3328030 <414069145@qq.com>
2021-04-08 21:54:13 +0800

    1

---
+++ src/components/notice/index.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>notice</div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+
+    },
+    mounted() {
+
+    }
+}
+</script>
+
+<style scoped>
+</style>
---
+++ src/utils/create.js
@@ -0,0 +1,5 @@
+// 创建指定组件实例并挂在body上
+
+export default function create(Component, props) {
+
+}
